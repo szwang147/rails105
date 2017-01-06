ff91d6e5c44ef3d1f87a504f526fd3eb194562eb
simon <wangzd.fudan@gmail.com>
2017-01-06 14:13:25 +0800

    take some flash notes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,8 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:notice] = 'welcome to the rails104 success'
+    flash[:warning] = 'welcome to the rails104 warning'
+    flash[:alert] = 'welcome to the rails104 danger'
+  end
+
 end
