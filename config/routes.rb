de65f56b1b0c60421d4369aef178f720b7413835
simon <wangzd.fudan@gmail.com>
2017-01-06 15:53:31 +0800

    change the root for groups index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  root 'welcome#index'
+  # root 'welcome#index'
+
+
+    root 'groups#index'
   resources :groups
   # 顺序无所谓的
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
