a5940a0da561531553946670bd5dd28bcc272989
simon <wangzd.fudan@gmail.com>
2017-01-06 15:52:16 +0800

    create group index and test some change

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
   root 'welcome#index'
+  resources :groups
+  # 顺序无所谓的
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
