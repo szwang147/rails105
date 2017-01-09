c140fb7a1499dc823ce8ec257554d604ae6bc457
simon <wangzd.fudan@gmail.com>
2017-01-09 20:49:09 +0800

    implement my post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,14 +8,10 @@ end
 def create
   @group = Group.find(params[:group_id])
   @post = Post.new(post_params)
-
   @post.group = @group
 # @post.group_id = @group.id
-
   @post.user = current_user
 # @post.user_id = current_user.id
-
-
   if @post.save
     redirect_to group_path(@group)
   else
@@ -23,7 +19,27 @@ def create
   end
 end
 
+def edit
+  @group = Group.find(params[:group_id])
+  @post = Post.find(params[:id])
+end
 
+def update
+  @group = Group.find(params[:group_id])
+  @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "update success"
+    else
+      render :edit
+    end
+end
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+  flash[:alert] = "post deleted"
+  redirect_to account_posts_path
+end
 
 private
 def post_params
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
 
   namespace :account do
     resources :groups
+    resources :posts
   end
   # 顺序无所谓的
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
